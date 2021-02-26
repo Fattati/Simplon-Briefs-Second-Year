1aae25201c2117f777c50d29c2a4e5f3e6f6baa3
Fattati <fattatireda@gmail.com>
2021-02-26 13:36:45 +0100

    update from HOME

--- VeterinaryBrief/app/src/util/HibernateUtil.java
+++ VeterinaryBrief/app/src/util/HibernateUtil.java
@@ -1,5 +1,37 @@
 package util;
 
-public class HibernateUtil {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+import beans.User;
+
+public class HibernateUtil {
+	private static SessionFactory factory;
+	
+	private HibernateUtil() {
+		
+	}
+	public static SessionFactory getSessionFactory() {
+		if (factory == null) {
+			Configuration config = new Configuration().configure("hibernate.cfg.xml");
+			factory = setupClasses(config).buildSessionFactory();
+		}
+		return factory;
+	}
+	
+	private static Configuration setupClasses(Configuration config) {
+        config.addAnnotatedClass(User.class);
+        
+        return config;
+    }
+	
+	public static Session openSession() {
+        return getSessionFactory().openSession();
+    }
+
+    public static void closeSession() {
+        getSessionFactory().close();
+    }
+    
 }
