30dea06b3dd494d345a01b95eb437dbf856efa3d
Fattati <fattatireda@gmail.com>
2021-04-11 15:26:19 +0000

    first step

---
+++ Brief_13/src/main/java/com/springboot/service/UserService.java
@@ -0,0 +1,12 @@
+package com.springboot.service;
+
+import com.springboot.model.User;
+import com.springboot.model.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    User save(UserDto user);
+    List<User> findAll();
+    User findOne(String username);
+}
