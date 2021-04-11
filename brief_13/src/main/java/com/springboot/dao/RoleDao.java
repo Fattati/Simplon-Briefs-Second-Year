805ec5077ba7ae095193009504e99ae3463a8bfa
Fattati <fattatireda@gmail.com>
2021-04-11 15:29:53 +0000

    saisho no ippo

---
+++ brief_13/src/main/java/com/springboot/dao/RoleDao.java
@@ -0,0 +1,10 @@
+package com.springboot.dao;
+
+import com.springboot.model.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleDao extends CrudRepository<Role, Long> {
+    Role findRoleByName(String name);
+}
---
+++ brief_13/src/main/java/com/springboot/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.springboot.dao;
+
+import com.springboot.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+}
